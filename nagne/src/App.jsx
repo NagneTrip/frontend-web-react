c813d203ea43c6c505febcb043ed7617a97c0d1a
kangcw0107 <kangcw0107@gmail.com>
2024-04-22 17:22:59 +0900

    라우트에 Main페이지 추가함

--- nagne/src/App.jsx
+++ nagne/src/App.jsx
@@ -1,16 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
+import Main from "./page/Main";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Routes>{/* <Route path="/" element={</>}></Route> */}</Routes>
-      <div className="main">
-        <div className="main-content"></div>
-        <div className="side-bar"></div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
     </div>
   );
 }
