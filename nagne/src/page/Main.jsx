d89e92150b5fd7a047fd3abaed89f7706f0f18b5
kangcw0107 <kangcw0107@gmail.com>
2024-04-22 17:24:30 +0900

    Main 페이지 레이아웃 & 스타일 수정

---
+++ nagne/src/page/Main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      {/* 메인 영역 */}
+      <div className="main-container">
+        <div className="bookmark"></div>
+        <div className="wrapper">
+          <div className="content">
+            <div className="write-article-box"></div>
+            <div className="article-container">
+              {/* <Article />
+            <Article />
+            <RecomendFriends />
+            <Article />
+            <Advertisement /> */}
+            </div>
+          </div>
+
+          {/* 사이드바 영역 */}
+          <div className="side">
+            <div className="side-weather"></div>
+            <div className="side-notice"></div>
+            <div className="side-bestarticle"></div>
+            <div className="side-footer"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
