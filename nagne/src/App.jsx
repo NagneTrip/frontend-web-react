116360be7135e764efcaa9ea6f908da18a46c162
kangcw0107 <kangcw0107@gmail.com>
2024-04-26 16:22:29 +0900

    날씨 로딩 스피너 추가

--- nagne/src/App.jsx
+++ nagne/src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
-import Main from "./page/Main";
+import Mainpage from "./page/Mainpage";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Routes>
-        <Route path="/" element={<Main />}></Route>
+        <Route path="/" element={<Mainpage />}></Route>
       </Routes>
     </div>
   );
